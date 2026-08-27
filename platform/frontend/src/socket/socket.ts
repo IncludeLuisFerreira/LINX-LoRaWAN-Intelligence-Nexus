c1c22042930d13c7d183b3be2f2cfeee2d20936b
includeLuisFerreira <prgluisfelipe@gmail.com>
2026-08-26 23:34:20 -0300

    Exclusão da pasta platform por ser redundante para o projeto

--- platform/frontend/src/socket/socket.ts
+++
@@ -1,9 +0,0 @@
-import { io, type Socket } from 'socket.io-client'
-
-let socket: Socket | null = null
-
-export function connectSocket(): Socket {
-  if (socket) return socket
-  socket = io()
-  return socket
-}
