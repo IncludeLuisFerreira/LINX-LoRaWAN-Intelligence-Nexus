02b1f0e9aeb44573893abbd4df083a055e0d7a7f
Nickael dos Reis <nickael@Nickael.nickka>
2026-09-18 12:37:36 -0300

    feat: adiciona o Dockerfile e o nginx.conf, com o acesso em localhost:8080

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,11 @@ import App from './App';
 import './index.css';
 
 async function prepareApp() {
-  if (import.meta.env.DEV) {
+  // Permite ativar o MSW se estiver em DEV ou se a flag VITE_ENABLE_MOCKS for 'true'
+  const shouldEnableMocks =
+    import.meta.env.DEV || import.meta.env.VITE_ENABLE_MOCKS === 'true';
+
+  if (shouldEnableMocks) {
     const { worker } = await import('./mocks/browser');
     return worker.start({ onUnhandledRequest: 'bypass' });
   }
